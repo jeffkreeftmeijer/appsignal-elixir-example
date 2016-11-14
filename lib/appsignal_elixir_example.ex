389ff95cbba2951f205903f0b5fcab8ccbcbc43b
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2016-11-14 11:43:53 +0100

    $ mix new appsignal_elixir_example

---
+++ lib/appsignal_elixir_example.ex
@@ -0,0 +1,18 @@
+defmodule AppsignalElixirExample do
+  @moduledoc """
+  Documentation for `AppsignalElixirExample`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AppsignalElixirExample.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/appsignal_elixir_example_test.exs
@@ -0,0 +1,8 @@
+defmodule AppsignalElixirExampleTest do
+  use ExUnit.Case
+  doctest AppsignalElixirExample
+
+  test "greets the world" do
+    assert AppsignalElixirExample.hello() == :world
+  end
+end
