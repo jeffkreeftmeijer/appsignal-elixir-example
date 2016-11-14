ddc67c969846f9e64f138f1adac1d8ba5064eec1
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2016-11-14 11:46:54 +0100

    Add AppSignal as per https://hexdocs.pm/appsignal/gettingstarted.html#installation

--- config/config.exs
+++ config/config.exs
@@ -28,3 +28,7 @@ use Mix.Config
 # here (which is why it is important to import them last).
 #
 #     import_config "#{Mix.env}.exs"
+
+config :appsignal, :config,
+  name: :appsignal_elixir_example,
+  push_api_key: "00000000-0000-0000-0000-000000000000"
