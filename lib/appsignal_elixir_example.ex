4f39b8c06b944e304169203abca6b11fca5eb2ee
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2017-02-17 09:54:04 +0100

    Add decorated "backgound job"

--- lib/appsignal_elixir_example.ex
+++ lib/appsignal_elixir_example.ex
@@ -1,2 +1,13 @@
 defmodule AppsignalElixirExample do
+  use Appsignal.Instrumentation.Decorators
+
+  @decorate transaction(:background_job)
+  def my_background_job do
+    my_function()
+  end
+
+  @decorate transaction_event()
+  def my_function do
+    :timer.sleep(1000)
+  end
 end
