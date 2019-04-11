549440b6fa0b2ee585cb86e64ba6eaf6d8979d5d
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2019-04-11 17:09:32 +0200

    Add AppsignalElixirExample.instrumentation/0

--- lib/appsignal_elixir_example.ex
+++ lib/appsignal_elixir_example.ex
@@ -1,2 +1,17 @@
 defmodule AppsignalElixirExample do
+  import Appsignal.Instrumentation.Helpers, only: [instrument: 4]
+
+  def instrumentation do
+    transaction =
+      Appsignal.Transaction.generate_id()
+      |> Appsignal.Transaction.start(:http_request)
+      |> Appsignal.Transaction.set_action("AppsignalElixirExample.instrumentation")
+
+    instrument(transaction, "query.posts", "Fetching all posts", fn ->
+      :timer.sleep(1000)
+    end)
+
+    Appsignal.Transaction.finish(transaction)
+    :ok = Appsignal.Transaction.complete(transaction)
+  end
 end
